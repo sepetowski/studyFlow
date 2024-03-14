5beb1ec2dc6af1095138b16d598aa980b976d270
bushmeen <bushmeen@gmail.com>
2024-03-14 20:33:48 +0100

    made chat real time

--- components/chat/messages/MessagesContainer.tsx
+++ components/chat/messages/MessagesContainer.tsx
@@ -6,6 +6,14 @@ import { Message } from './Message';
 import { useMessage } from '@/store/conversation/messages';
 import { LoadingState } from '@/components/ui/loading-state';
 import { DeleteMessage } from './DeleteMessage';
+import axios, { AxiosError } from 'axios';
+import { ExtendedMessage } from '@/types/extended';
+import { domain } from '@/lib/api';
+import {
+	RealtimePostgresDeletePayload,
+	RealtimePostgresInsertPayload,
+	RealtimePostgresUpdatePayload,
+} from '@supabase/supabase-js';
 
 interface Props {
 	workspaceId: string;
@@ -17,7 +25,11 @@ export const MessagesContainer = ({ chatId, workspaceId, sessionUserId }: Props)
 	const scrollRef = useRef() as React.MutableRefObject<HTMLDivElement>;
 	const [userScrolled, setUserScrolled] = useState(false);
 
-	const { messages, initialMessagesLoading } = useMessage((state) => state);
+	const [notification, setNotification] = useState(0);
+
+	const { messages, initialMessagesLoading, addMessage, editMessage, deleteMessage } = useMessage(
+		(state) => state
+	);
 
 	useEffect(() => {
 		const scrollContainer = scrollRef.current;
@@ -26,24 +38,96 @@ export const MessagesContainer = ({ chatId, workspaceId, sessionUserId }: Props)
 		}
 	}, [messages, userScrolled]);
 
-	// useEffect(() => {
-	//     const supabaseClient = supabase();
-	// 	const channel = supabaseClient
-	// 		.channel(`chat-${chatId}`)
-	// 		.on(
-	// 			'postgres_changes',
-	// 			{ event: 'INSERT', schema: 'public', table: 'Message' },
-	// 			(payload) => {
-	// 				console.log('Change received!', payload);
-	// 			}
-	// 		)
-	// 		.subscribe();
+	useEffect(() => {
+		const supabaseClient = supabase();
 
-	// 	return () => {
-	// 		channel.unsubscribe();
-	// 	};
-	// }, [chatId]);
+		const handleAddMessage = async (
+			payload: RealtimePostgresInsertPayload<{
+				[key: string]: any;
+			}>
+		) => {
+			if (sessionUserId !== payload.new.senderId) {
+				try {
+					const { data, status } = await axios.get<ExtendedMessage>(
+						`${domain}/api/conversation/get/new_message?messageId=${payload.new.id}`
+					);
+					if (data) addMessage(data);
 
+					const scrollContainer = scrollRef.current;
+					if (
+						scrollContainer.scrollTop <
+						scrollContainer.scrollHeight - scrollContainer.clientHeight - 10
+					) {
+						setNotification((current) => current + 1);
+					}
+				} catch (err) {
+					console.log('error');
+				}
+			}
+		};
+
+		const handleUpdateMessage = (
+			payload: RealtimePostgresUpdatePayload<{
+				[key: string]: any;
+			}>
+		) => {
+			if (sessionUserId !== payload.new.senderId) {
+				editMessage(payload.new.id, payload.new.content);
+			}
+		};
+
+		const handleDeleteMessage = (
+			payload: RealtimePostgresDeletePayload<{
+				[key: string]: any;
+			}>
+		) => {
+			const messageExists = messages.find((message) => message.id === payload.old.id);
+			if (messageExists) deleteMessage(payload.old.id);
+		};
+
+		const channel = supabaseClient
+			.channel(`chat-${chatId}`)
+			.on(
+				'postgres_changes',
+				{ event: 'INSERT', schema: 'public', table: 'Message' },
+				handleAddMessage
+			)
+			.on(
+				'postgres_changes',
+				{ event: 'UPDATE', schema: 'public', table: 'Message' },
+				handleUpdateMessage
+			)
+			.on(
+				'postgres_changes',
+				{ event: 'DELETE', schema: 'public', table: 'Message' },
+				handleDeleteMessage
+			)
+			.subscribe();
+
+		return () => {
+			channel.unsubscribe();
+		};
+
+		// eslint-disable-next-line
+	}, [chatId, messages, sessionUserId]);
+
+	const handleOnScroll = () => {
+		const scrollContainer = scrollRef.current;
+		if (scrollContainer) {
+			const isScroll =
+				scrollContainer.scrollTop <
+				scrollContainer.scrollHeight - scrollContainer.clientHeight - 10;
+			setUserScrolled(isScroll);
+			if (
+				scrollContainer.scrollTop ===
+				scrollContainer.scrollHeight - scrollContainer.clientHeight
+			) {
+				setNotification(0);
+			}
+		}
+	};
+
+	console.log(notification);
 	if (initialMessagesLoading)
 		<div className='h-full flex flex-col items-center  justify-center'>
 			<LoadingState />
@@ -51,6 +135,7 @@ export const MessagesContainer = ({ chatId, workspaceId, sessionUserId }: Props)
 	return (
 		<div
 			ref={scrollRef}
+			onScroll={handleOnScroll}
 			className='h-full flex flex-col gap-2 px-4 py-2 overflow-y-auto scrollbar-thin scrollbar-thumb-secondary scrollbar-track-background '>
 			{messages.map((message) => (
 				<Message key={message.id} message={message} sessionUserId={sessionUserId} />
